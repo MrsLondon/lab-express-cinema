2ae3afb6edf4173961705096027c0a778c133a2f
ayat <abuhajayat@gmail.com>
2025-03-01 08:58:09 +0100

    halfway

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model"); 
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', async (req, res, next) => { 
+  try {
+    const movies = await Movie.find(); // Fetch all movies from MongoDB
+    res.render("index", { 
+      title: "Cinema Ironhack", // Passing "Cinema Ironhack" as the value for title
+      movies: movies // Pass movies data to the template
+    }); 
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    next(error);
+  }
+});
 
 module.exports = router;
